9ad2dc1e7c41892ce6677271cfcf534138694192
Travis T <travistokuyama@gmail.com>
2019-01-28 19:47:06 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -12,9 +12,14 @@ const BUTCHER_PRODUCTS = [
 export class OlderCoaster extends Component {
   render() {
     return (
-      <>
-        
-      </p>
+      <div className="oldercoaster">
+        <p>Two grannies having the time of their life!</p>
+        <p>Passengers:</p>
+        <ul>
+          <li>Agnes</li>
+          <li>Muriel</li>
+        </ul>
+      </div>
     )
   }
  
