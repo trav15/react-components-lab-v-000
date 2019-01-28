1ef786eeacaaa265d685ff610cd4ed78d0317d82
Travis T <travistokuyama@gmail.com>
2019-01-28 19:44:38 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -11,13 +11,7 @@ const BUTCHER_PRODUCTS = [
 // Define these exported classes
 export class OlderCoaster extends Component {
 
-  // this function should return the JSX we want the DOM to render
-  // render() {
-  //   return (
-  //     <div>Hello!</div>
-  //   )
-  // }
-  //
+ 
 }
 
 export class InFrontOfYou extends Component {
