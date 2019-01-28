8ae42912264f01c21a4d97cceab297df3a122b82
Travis T <travistokuyama@gmail.com>
2019-01-28 19:50:00 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -53,7 +53,9 @@ export class App extends Component {
   render() {
     return (
       <div id="app">
-
+        <OlderCoaster />
+        <InFrontOfYou />
+        <ButcherShop />
       </div>
     )
   }
